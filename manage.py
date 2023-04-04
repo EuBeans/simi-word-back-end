68a52a67b9ed3f012a4f37c81b4f3ae46bc1cb38
Jean Sfeir <43392082+EuBeans@users.noreply.github.com>
2023-04-03 20:54:46 -0400

    after request Origin access added

--- manage.py
+++ manage.py
@@ -26,6 +26,13 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 
 @manager.command
 def run():
